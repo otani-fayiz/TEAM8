0982b05cab44ca1b5f37147f3052dc61a82316d4
Fayiz Asad <fayiz@otani-trading.com>
2024-12-10 10:53:56 +0400

    Small fixes adn build test

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,73 @@
+import { Link, NavLink } from 'react-router-dom';
+import { assets } from '../assets/assets';
+import { useContext, useState } from 'react';
+import { ShopContext } from '../Context/ShopContext';
+
+const Navbar = () => {
+  const [visible, setVisible] = useState(false);
+  const { setShowSearch, getCartCount } = useContext(ShopContext);
+
+  return (
+    <div className="flex items-center justify-between py-5 font-medium">
+      <Link to="/">
+        <img src={assets.logo} alt="logo" className="w-36" />
+      </Link>
+
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+        <NavLink to="/" className="flex flex-col items-center gap-1">
+          <p>HOME</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/collection" className="flex flex-col items-center gap-1">
+          <p>COLLECTION</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/about" className="flex flex-col items-center gap-1">
+          <p>ABOUT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/contact" className="flex flex-col items-center gap-1">
+          <p>CONTACT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+      </ul>
+
+      <div className="flex items-center gap-6">
+        <img
+          onClick={() => {
+            setShowSearch(true);
+          }}
+          src={assets.search_icon}
+          alt=""
+          className="w-5 cursor-pointer "
+        />
+        <div className="group relative">
+          <Link to="/login">
+            <img
+              src={assets.profile_icon}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          </Link>
+          
+          </div>
+        </div>
+
+        <Link to="/cart" className="relative ">
+          <img src={assets.cart_icon} alt="" className="w-5 min-w-5 " />
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+            {getCartCount()}
+          </p>
+        </Link>
+
+        <img
+          src={assets.menu_icon}
+          alt=""
+          className="w-5 cursor-pointer sm:hidden "
+          onClick={() => setVisible(!visible)} //visibility toggle (true/false))
+        />
+      </div>
+  );
+};
+
+export default Navbar;
